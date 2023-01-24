c437dd16911bc656e13aeaae83ef5ab32d7de252
Daria Korniienko <106033233+Dashasoham@users.noreply.github.com>
2023-01-24 13:26:10 +0000

    Logical Operators

---
+++ exercise.js
@@ -0,0 +1,65 @@
+/*
+
+// const findMin = (...args) => Math.min(...args);
+// findMin(3, 7, 14, 15);
+
+// const mergeObjects = (obj1, obj2) => ({ ...obj1, ...obj2 });
+// mergeObjects({ a: 1, b: 2 }, { c: 3, d: 4 }); // {a:1, b:2, c:3, d:4}
+
+// //_________________________________________
+// let fruits = ['Apple', 'Orange', 'Banana'];
+// let newFruitArray = [...fruits];
+
+// // console.log(newFruitArray);
+
+// let arr1 = ['A', 'B', 'C'];
+// let arr2 = ['X', 'Y', 'Z'];
+// let result = [...arr1, ...arr2];
+// // console.log(result); // ['A', 'B', 'C', 'X', 'Y', 'Z']
+
+// let newFruits = ['Cherry', ...fruits];
+// // console.log(newFruits); // ['Cherry', 'Apple','Orange','Banana']
+
+// let getFruits = (f1, f2, f3) => {
+//   console.log(`Fruits: ${f1}, ${f2} and ${f3}`);
+// };
+// getFruits(...fruits);
+
+// let obj1 = {
+//   id: 101,
+//   name: 'Jhon Doe',
+// };
+
+// let obj2 = {
+//   age: 25,
+//   country: 'USA',
+// };
+
+// const employee = { ...obj1, ...obj2 };
+// console.log(employee);
+
+// let prices = [12, 20, 18];
+// let newPrices = [...prices];
+// console.log(newPrices);
+// console.log(prices);
+
+// let alphabets = ['A', ...'BCD', 'E'];
+// console.log(alphabets);
+
+// var prices2 = [12, 20, 18];
+// var maxPrice = Math.max(...prices2);
+// console.log(maxPrice);
+
+// var newArray = Array(...[, ,]);
+// console.log(newArray);
+
+// var newArray2 = [...[, ,]];
+// console.log(newArray2);
+
+// Original array randomly generated
+
+let originalArray = [3, 5, 7, 9];
+console.log(originalArray);
+// let clonedArray = [1, ...originalArray];
+let [...clonedArray] = originalArray;
+*/
--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 'use strict';
 
-// Data needed for a later exercise
-// const flights =
-//   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+/*
+// // Data needed for a later exercise
+// // const flights =
+// //   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
 
-// Data needed for first part of the section
+// // Data needed for first part of the section
 const restaurant = {
   name: 'Classico Italiano',
   location: 'Via Angelo Tavanti 23, Firenze, Italy',
@@ -43,37 +44,72 @@ const restaurant = {
   orderPasta: function (ing1, ing2, ing3) {
     console.log(`Here´s your pasta with ${ing1}, ${ing2} and ${ing3}`);
   },
+  orderPizza: function (mainIngredient, ...otherIngredients) {
+    console.log(mainIngredient, otherIngredients);
+  },
 };
 
-const arr = [7, 8, 9];
-const badNewArray = [1, 2, arr[0], arr[2], arr[2]];
-console.log(badNewArray);
+// //1)DESTRUCTURING
+// const arr = [1, 2, ...[3, 4]];
+// console.log(arr);
 
-const goodNewArray = [1, 2, ...arr];
-console.log(goodNewArray);
+// const [a, b, ...others] = [1, 2, 3, 4, 5];
+// console.log(a, b, others);
 
-console.log(...goodNewArray);
-console.log(...restaurant.mainMenu);
+// const [pizza, , risotto, ...otherFood] = [
+//   ...restaurant.mainMenu,
+//   ...restaurant.starterMenu,
+// ];
+// console.log(pizza, risotto, otherFood);
 
-const newMenu = [...restaurant.mainMenu, 'Gnocci'];
-console.log(newMenu);
+// const { sat, ...weekdays } = restaurant.openingHours;
+// console.log(weekdays);
 
-//COPY ARRAY
+// //2)FUNCTIONS
+// const add = function (...numbers) {
+//   let sum = 0;
+//   for (let i = 0; i < numbers.length; i++) sum = sum + numbers[i];
+//   console.log(sum);
+// };
+// add(2, 3);
+// add(5, 3, 7, 2);
+// add(8, 2, 5, 3, 2, 1, 4);
 
-const mainMenuCopy = [...restaurant.mainMenu];
+// const x = [23, 5, 7];
+// add(...x);
 
-//JOIN 2 ARRAYS
-const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
-console.log(menu);
+// restaurant.orderPizza('mushrooms', 'corn', 'olives', 'spinach');
+// restaurant.orderPizza('corn');
+
+// // const arr = [7, 8, 9];
+// const badNewArray = [1, 2, arr[0], arr[2], arr[2]];
+// console.log(badNewArray);
+
+// const goodNewArray = [1, 2, ...arr];
+// console.log(goodNewArray);
+
+// console.log(...goodNewArray);
+// console.log(...restaurant.mainMenu);
+
+// const newMenu = [...restaurant.mainMenu, 'Gnocci'];
+// console.log(newMenu);
+
+// //COPY ARRAY
+
+// const mainMenuCopy = [...restaurant.mainMenu];
+
+// //JOIN 2 ARRAYS
+// const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
+// console.log(menu);
 
 //Iterables
-const str = 'Jonas';
-const letters = [...str, '', 'S.'];
-console.log(letters);
-console.log(...str);
-console.log(str[2]);
+// const str = 'Jonas';
+// const letters = [...str, '', 'S.'];
+// console.log(letters);
+// console.log(...str);
+// console.log(str[2]);
 
-//Real world example
+// Real world example
 // const ingridients = [
 //   prompt(`Let´s make pasta! Ingredient 1?`),
 //   prompt(`Let´s make pasta! Ingredient 2`),
@@ -83,17 +119,17 @@ console.log(str[2]);
 
 // restaurant.orderPasta(...ingridients);
 
-//OBJECTS
+// //OBJECTS
 
-const newRestaurant = { ...restaurant, founder: 'Giuseppe', foundedIn: 1998 };
-console.log(newRestaurant);
+// const newRestaurant = { ...restaurant, founder: 'Giuseppe', foundedIn: 1998 };
+// console.log(newRestaurant);
 
-const restaurantCopy = { ...restaurant };
-restaurantCopy.name = 'Ristorante Roma';
-console.log(restaurant.name);
-console.log(restaurantCopy.name);
+// const restaurantCopy = { ...restaurant };
+// restaurantCopy.name = 'Ristorante Roma';
+// console.log(restaurant.name);
+// console.log(restaurantCopy.name);
 
-//DESTRUCTURING OBJECTS
+// DESTRUCTURING OBJECTS
 
 // restaurant.orderDelivery({
 //   time: '22.30',
@@ -155,7 +191,7 @@ console.log(restaurantCopy.name);
 // [main, secondary] = [secondary, main];
 // console.log(main, secondary);
 
-//RECEIVE 2 RETURN VALUES FROM A FUNCTION
+// RECEIVE 2 RETURN VALUES FROM A FUNCTION
 // console.log(restaurant.order(2, 0));
 // const [starter, main] = restaurant.order(2, 0);
 // console.log(starter, main);
@@ -170,3 +206,116 @@ console.log(restaurantCopy.name);
 // //DEFAULT VALUES
 // const [p = 1, q = 1, r = 1] = [8];
 // console.log(p, q, r);
+
+console.log('-------OR--------');
+
+console.log(3 || 'Jonas');
+console.log('' || 'Jonas');
+console.log(true || 0);
+console.log(undefined || null);
+
+console.log(undefined || 0 || 'Hello' || 23 || null);
+
+restaurant.numGuests = 23;
+const guests1 = restaurant.numGuests ? restaurant.numGuests : 10;
+console.log(guests1);
+
+const guests2 = restaurant.numGuests || 10;
+console.log(guests2);
+
+console.log('-------AND--------');
+console.log(0 && 'Jonas');
+console.log(7 && 'Jonas');
+
+console.log('Hello' && 23 && null && 'jonas');
+
+if (restaurant.orderPizza) {
+  restaurant.orderPizza('mushrooms', 'spinach');
+}
+
+restaurant.orderPizza && restaurant.orderPizza('mushrooms', 'spinach');
+
+console.log('----The Nullish Coalescing Operator------');
+
+restaurant.numGuests = 0;
+
+const guests3 = restaurant.numGuests || 10;
+console.log(guests3);
+
+const guestsCorrect = restaurant.numGuests ?? 10;
+console.log(guestsCorrect);
+
+*/
+
+const restaurant = {
+  name: 'Classico Italiano',
+  location: 'Via Angelo Tavanti 23, Firenze, Italy',
+  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+  openingHours: {
+    thu: {
+      open: 12,
+      close: 22,
+    },
+    fri: {
+      open: 11,
+      close: 23,
+    },
+    sat: {
+      open: 0, // Open 24 hours
+      close: 24,
+    },
+  },
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+
+  orderDelivery: function ({
+    starterIndex = 1,
+    mainIndex = 0,
+    time = '20.00',
+    address,
+  }) {
+    console.log(
+      `Order received!${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`
+    );
+  },
+
+  orderPasta: function (ing1, ing2, ing3) {
+    console.log(`Here´s your pasta with ${ing1}, ${ing2} and ${ing3}`);
+  },
+  orderPizza: function (mainIngredient, ...otherIngredients) {
+    console.log(mainIngredient, otherIngredients);
+  },
+};
+
+const rest1 = {
+  name: 'Capri',
+  //   numGuests: 20,
+
+  numGuests: 0,
+};
+
+const rest2 = {
+  name: 'La Piazza',
+  owner: 'Giovanni',
+};
+// "OR" Assignment Operator
+// rest2.numGuests = rest2.numGuests || 10;
+// rest1.numGuests = rest1.numGuests || 10;
+
+rest1.numGuests ||= 10;
+rest2.numGuests ||= 10;
+
+//Nullish Assignment operator
+rest1.numGuests ??= 10;
+rest2.numGuests ??= 10;
+
+rest2.owner = rest2.owner && '<ANONYMOUS>';
+// rest1.owner = rest1.owner && `<ANONYMOUS>`;
+
+rest1.owner &&= '<ANONYMOUS>';
+
+console.log(rest1);
+console.log(rest2);
