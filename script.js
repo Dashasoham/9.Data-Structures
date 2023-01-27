822a5a9cddffbbb370bd4eb0197be922243e2b28
Daria Korniienko <106033233+Dashasoham@users.noreply.github.com>
2023-01-27 15:01:18 +0000

    maps

--- script.js
+++ script.js
@@ -468,3 +468,37 @@ console.log(rest.size);
 console.log(rest.get(arr));
 
 console.log('----MAPS ITERATION----');
+
+const question = new Map([
+  ['question', 'What is the best language?'],
+  [1, 'C'],
+  [2, 'Java'],
+  [3, 'JavaScript'],
+  ['correct', 3],
+  [true, 'Correct'],
+  [false, 'Try again'],
+]);
+console.log(question);
+
+//CONVERT OBJECT TO MAP
+console.log(Object.entries(openingHours));
+const hoursMap = new Map(Object.entries(openingHours));
+
+console.log(hoursMap);
+
+//FOR LOOP
+console.log(question.get('question'));
+for (const [key, value] of question) {
+  if (typeof key === 'number') console.log(`Answer ${key}:${value}`);
+}
+
+const answer = Number(prompt('Your answer'));
+console.log(answer);
+
+answer === question.get('correct')
+  ? console.log(question.get(true))
+  : console.log(question.get(false));
+
+//COnvert map to array
+
+console.log([...question]);
