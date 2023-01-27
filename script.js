4af5d59db14dbba92db821690530edf99850ad05
Daria Korniienko <106033233+Dashasoham@users.noreply.github.com>
2023-01-27 13:48:02 +0000

    maps

--- script.js
+++ script.js
@@ -430,3 +430,41 @@ console.log(
 console.log(new Set(staff));
 
 console.log(new Set('daria').size);
+
+console.log('----MAPS-----');
+
+const rest = new Map();
+rest.set('name', 'Classico Italiano');
+
+rest.set(1, 'Milano,Italy');
+rest.set(2, 'Lisbon,Portugal');
+console.log(rest.set(2, 'Lisbon,Portugal'));
+
+rest
+  .set('categories', ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'])
+  .set('open', 11)
+  .set('close', 23)
+  .set(true, 'We are open')
+  .set(false, 'We are closed');
+console.log(rest.get('name'));
+
+const time = 21;
+console.log(rest.get(time > rest.get('open') && time < rest.get('close')));
+
+console.log(rest.has('categories'));
+console.log(rest.set(2, 'Lisbon,Portugal'));
+
+rest.delete(2);
+
+// rest.clear();
+const arr = [1, 2];
+rest.set(arr, 'Test');
+rest.set(document.querySelector('h1'), 'Heading');
+
+console.log(rest);
+
+console.log(rest.size);
+
+console.log(rest.get(arr));
+
+console.log('----MAPS ITERATION----');
