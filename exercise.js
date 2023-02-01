7b09261410f5d143b95cf2f9725144e8f4f0fc19
Daria Korniienko <106033233+Dashasoham@users.noreply.github.com>
2023-02-01 13:11:04 +0000

    String Method practice

--- exercise.js
+++ exercise.js
@@ -89,7 +89,7 @@ document.querySelector('button').addEventListener('click', function () {
     console.log(`${output.padEnd(20)}${'✅'.repeat(i + 1)}`);
   }
 });
-*/
+
 
 const sentence = 'The quick brown fox jumps over the lazy dog.';
 console.log(sentence.length);
@@ -129,3 +129,23 @@ console.log(namesUpper.join(' '));
 //     second[0],
 //     second[0].toUpperCase()
 //   )}`
+*/
+const flights =
+  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+
+console.log(flights.split('+'));
+
+const getCode = str => str.slice(0, 3).toUpperCase();
+
+for (const flight of flights.split('+')) {
+  const [type, from, to, time] = flight.split(';');
+  // console.log(flight.split(';'));
+  const output = `${type.startsWith('_Delayed') ? '🔴' : ''}${type.replaceAll(
+    '_',
+    ' '
+  )} from ${getCode(from)} to ${getCode(to)} ${time.replace(
+    ':',
+    'H'
+  )}`.padStart(45);
+  console.log(output);
+}
